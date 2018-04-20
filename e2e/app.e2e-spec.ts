fa1aa07de6e31195dfccd7186f885a391437c447
SergiyLichenko <sergiy.lichenko@globallogic.com>
2018-04-20 14:36:36 +0300

    Added E2E tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,45 @@
 import { AppPage } from './app.po';
+import { browser, by } from 'protractor';
 
 describe('payoneer App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
+
+    browser.ignoreSynchronization = true;
+    browser.waitForAngular();
+    browser.sleep(500);
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should check initial count of table rows', async () => {
+    //Arrange
+    await page.navigateTo();
+
+    //Act
+    let tableRows = await page.getTableRowsAsync();
+
+    //Assert
+    expect(tableRows.length).toBe(4);
+  });
+
+  it('should filter by pending payments', async () => {
+    //Arrange
+    await page.navigateTo();
+    let pendingCheckbox = await page.getPendingPaymentCheckboxAsync();
+
+    //Act
+    await pendingCheckbox.click();
+
+    //Assert
+    let tableRows = await page.getTableRowsAsync();
+
+    let pendingSpan = tableRows[0].element(by.css('mat-cell:nth-of-type(2) span'));
+    let text = await pendingSpan.getText();
+    expect(text).toBe('Pending');
+
+    pendingSpan = tableRows[1].element(by.css('mat-cell:nth-of-type(2) span'));
+    text = await pendingSpan.getText();
+    expect(text).toBe('Pending');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,12 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  async getPendingPaymentCheckboxAsync() {
+    let checkboxes = await element.all(by.css('mat-checkbox'));
+    return checkboxes[0];
+  }
+
+  async getTableRowsAsync() {
+    return await element.all(by.css('mat-table mat-row'));
   }
 }
